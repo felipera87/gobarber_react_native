0c2f050de0cf79a9d9095e1a2edc8e963e8f3175
Felipe Reis de Andrade <felipe.reis@interlinkbrasil.com>
2021-02-17 17:58:22 -0300

    Created styles for components Input and Button

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { TextInputProps } from 'react-native';
+
+import { Container, TextInput, Icon } from './styles';
+
+interface InputProps
+  extends Omit<
+    TextInputProps,
+    'style' | 'selectionColor' | 'underlineColorAndroid'
+  > {
+  name: string;
+  icon: string;
+}
+
+const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => (
+  <Container>
+    <Icon name={icon} size={20} color="#666360" />
+    <TextInput
+      keyboardAppearance="dark"
+      {...rest}
+      placeholderTextColor="#666360"
+    />
+  </Container>
+);
+
+export default Input;
